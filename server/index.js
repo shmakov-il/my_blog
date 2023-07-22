14f517c1b6bdea23ca769e0821f9ffa231ab7335
Shmakov Ilya <il.e.shmakov@gmail.com>
2023-07-22 16:15:29 +0300

    start server and connect DB

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+
+
+// Константы из .env
+const PORT = process.env.PORT;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_NAME;
+
+
+async function start() {
+    try {
+        await mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.vqfvho4.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`);
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+    } catch (error) {
+        console.log(error);
+    }
+}
+start();
